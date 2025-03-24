9cef39c49c10d21d1b1d91c032cffb19f4330135
Jacob Mathew <168559645+jacobpmath@users.noreply.github.com>
2025-03-24 10:38:41 -0500

    added logic for those reviewing method

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -7,7 +7,10 @@ public class BankAccount {
 	public BankAccount() {
 		this.balance = 0;
 	}
-	
+	/**
+     	Deposits the given amount into the account.
+     	@param amount must be non-negative.
+     	*/
 	public void deposit(double amount) {
 		if(amount < 0) {
 			throw new IllegalArgumentException();
